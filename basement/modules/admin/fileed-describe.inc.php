76e087c4a0dce968718ae8c2b0acbf27463d92e8
werner-a <chaot@chaos.de>
2003-11-15 10:02:30 +0000

    bugfix fuer den bugfix :)

--- basement/modules/admin/fileed-describe.inc.php
+++ basement/modules/admin/fileed-describe.inc.php
@@ -169,7 +169,7 @@
                             img_resize( $file_org, $file_id, $img_src, $cfg["size"]["thumb"], $cfg["file"]["maindir"].$cfg["file"]["picture"]."thumbnail", "tn" );
 
                             // orginal bild nach max resizen oder loeschen
-                            if ( $cfg["size"]["max"] == "" || imagesx($img_src) <= $cfg["size"]["max"] || imagesy($img_src) <= $cfg["size"]["max"] ) {
+                            if ( $cfg["size"]["max"] == "" || imagesx($img_src) <= $cfg["size"]["max"] && imagesy($img_src) <= $cfg["size"]["max"] ) {
                                 rename($file_org,$cfg["file"]["maindir"].$cfg["file"]["picture"].$pathvars["filebase"]["pic"]["o"]."img_".$file_id.".png");
                             } else {
                                 img_resize( $file_org, $file_id, $img_src, $cfg["size"]["max"], $cfg["file"]["maindir"].$cfg["file"]["picture"]."original", "img" );
