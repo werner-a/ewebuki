ca468e0196d2956ec3c3c7315ae69776b44b0844
werner-a <chaot@chaos.de>
2004-10-06 18:03:35 +0000

    achtung das config file hat sich gaendert! der hide schalter wird beachtet multi db support aussehen des symbol kann angepasst werden das aussehen der links kann angepasst werden der taregt kann angepasst werden der basis path base kann angepasst werden support fuer "original template find" der sql befehl ist  nun sauber formatiert

--- basement/basic/libraries/menu.inc.php
+++ basement/basic/libraries/menu.inc.php
@@ -44,17 +44,33 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ** $script_name ** ]".$debugging["char"];
-    // mandatory
-    if ( $cfg["menu"]["level1"]["full"] == "-1" ) {
-        $mandatory = "";
-    } else {
-        $mandatory = " AND ((".$cfg["menu"]["db"]["entries"].".mandatory)='-1')";
-    }
+
+    //
     // menupunkte level 1
-    $sql = "SELECT ".$cfg["menu"]["db"]["entries"].".mid, ".$cfg["menu"]["db"]["entries"].".entry, ".$cfg["menu"]["db"]["entries"].".refid, ".$cfg["menu"]["db"]["entries"].".level, ".$cfg["menu"]["db"]["entries"]."_lang.lang, ".$cfg["menu"]["db"]["entries"]."_lang.label, ".$cfg["menu"]["db"]["entries"]."_lang.exturl FROM ".$cfg["menu"]["db"]["entries"]." INNER JOIN ".$cfg["menu"]["db"]["entries"]."_lang ON ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["entries"]."_lang.mid WHERE (((".$cfg["menu"]["db"]["entries"].".refid)=0) AND ((".$cfg["menu"]["db"]["entries"]."_lang.lang)='".$environment["language"]."') ".$mandatory.") order by sort, label;";
-    #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level0sql: ".$sql.$debugging["char"];
+    //
+    $mandatory = " AND ((".$cfg["menu"]["db"]["entries"].".mandatory)='-1')";
+    if ( $cfg["menu"]["level1"]["full"] == "-1" ) $mandatory = "";
+
+    $sql = "SELECT  ".$cfg["menu"]["db"]["entries"].".mid,
+                    ".$cfg["menu"]["db"]["entries"].".entry,
+                    ".$cfg["menu"]["db"]["entries"].".refid,
+                    ".$cfg["menu"]["db"]["entries"].".level,
+                    ".$cfg["menu"]["db"]["entries"]."_lang.lang,
+                    ".$cfg["menu"]["db"]["entries"]."_lang.label,
+                    ".$cfg["menu"]["db"]["entries"]."_lang.exturl
+              FROM  ".$cfg["menu"]["db"]["entries"]."
+        INNER JOIN  ".$cfg["menu"]["db"]["entries"]."_lang
+                ON  ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["entries"]."_lang.mid
+             WHERE (
+                   (".$cfg["menu"]["db"]["entries"].".refid=0)
+               AND (".$cfg["menu"]["db"]["entries"].".hide <> '-1' OR ".$cfg["menu"]["db"]["entries"].".hide IS NULL)
+               AND (".$cfg["menu"]["db"]["entries"]."_lang.lang='".$environment["language"]."')
+               ".$mandatory."
+                   )
+          ORDER BY sort, label;";
     $level1result  = $db -> query($sql);
-    #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level0res: ".$submenuresult.$debugging["char"];
+    if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level1sql: ".$sql.$debugging["char"];
+    if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level1res: ".$level1result.$debugging["char"];
 
     // entweder alle in anderer sprache oder nichts
     if ( $db -> num_rows($level1result) == 0 ){
@@ -79,37 +95,77 @@
                 }
             }
             if ( $right == -1 ) {
-                // mandatory
+                // die boese schneide ab funktion
+                if ( strlen($level1array["label"]) > $cfg["menu"]["level1"]["length"] ) {
+                    $level1array["label"] = substr($level1array["label"],0,$cfg["menu"]["level1"]["length"]-4)." ...";
+                }
                 $mandatory = " AND ((".$cfg["menu"]["db"]["entries"].".mandatory)='-1')";
                 if ( $cfg["menu"]["level1"]["force"] == -1 ) $mandatory = "";
-                if ( strpos($environment["ebene"],$level1array["entry"]) == 1 || ( $environment["kategorie"] == $level1array["entry"] && $environment["ebene"] == "" ) ) {
-                    // ???open.png
-                    $ausgaben["ordner"] = "<a class=\"".$cfg["menu"]["level1"]["style"]."\" href=\"".$pathvars["virtual"]."/".$level1array["entry"].".html\"><img src=\"../../images/".$environment["design"]."/".$cfg["menu"]["image"]."open.png\" width=\"16\" height=\"16\" align=\"bottom\" border=\"0\" alt=\"".$level1array["label"]."\"></a>";
-                    // mandatory
+
+                // wo geht der href hin?
+                if ( $level1array["exturl"] == "" ) {
+                    $href = $cfg["menu"]["base"]."/".$level1array["entry"].".html";
+                    $target = "";
+                } else {
+                    $href = $level1array["exturl"];
+                    $target = $cfg["menu"]["level1"]["target"];
+                }
+                $marken = array("##target##", "##link##", "##label##");
+                $ersatz = array($target, $href, $level1array["label"]);
+
+                // multiple db support
+                if ( $cfg["menu"]["mdbsupp"] == -1 ) {
+                  $aktdb = $db->getDb();
+                  if ( $environment["fqdn"][0] == $cfg["menu"]["mdbname"] ) {
+                      $aktlev = DATABASE;
+                  } else {
+                      $aktlev = $environment["fqdn"][0];
+                  }
+                }
+
+                if ( strpos($environment["ebene"],$level1array["entry"]) == 1 && $aktlev == $aktdb || ( $environment["kategorie"] == $level1array["entry"] && $environment["ebene"] == "" && $aktlev == $aktdb ) ) {
+                    // open folder
+                    $ausgaben["ordner"] = str_replace($marken,$ersatz,$cfg["menu"]["level1"]["icona"]);
+
                     if ( $cfg["menu"]["level2"]["full"] == -1 ) $mandatory = "";
                     if ( $cfg["menu"]["level2"]["dynamic"] == -1 ) $cfg["menu"]["level2"]["enable"] = -1;
                 } else {
-                    // ???close.png
-                    $ausgaben["ordner"] = "<a class=\"".$cfg["menu"]["level1"]["style"]."\" href=\"".$pathvars["virtual"]."/".$level1array["entry"].".html\"><img src=\"../../images/".$environment["design"]."/".$cfg["menu"]["image"]."close.png\" width=\"16\" height=\"16\" align=\"bottom\" border=\"0\" alt=\"".$level1array["label"]."\"></a>";
+                    // closed folder
+                    $ausgaben["ordner"] = str_replace($marken,$ersatz,$cfg["menu"]["level1"]["iconb"]);
+
                     if ( $cfg["menu"]["level2"]["dynamic"] == -1 ) $cfg["menu"]["level2"]["enable"] = 0;
                 }
-                if ( $level1array["exturl"] == "" ) {
-                    $ausgaben["ueberschrift"] = "<a class=\"".$cfg["menu"]["level1"]["style"]."\" href=\"".$pathvars["virtual"]."/".$level1array["entry"].".html\">".$level1array["label"]."</a><br>";
-                } else {
-                    $ausgaben["ueberschrift"] = "<a class=\"".$cfg["menu"]["level1"]["style"]."\" target=\"_blank\" href=\"".$level1array["exturl"]."\">".$level1array["label"]."</a><br>";
-                }
+                $ausgaben["ueberschrift"] = str_replace($marken,$ersatz,$cfg["menu"]["level1"]["link"]);
             }
         } else {
             $ausgaben["ordner"] = "";
             $ausgaben["ueberschrift"] = "";
         }
 
-
+        //
         // menupunkte level 2
-        $sql = "SELECT ".$cfg["menu"]["db"]["entries"].".mid, ".$cfg["menu"]["db"]["entries"].".entry, ".$cfg["menu"]["db"]["entries"].".refid, ".$cfg["menu"]["db"]["entries"].".sort, ".$cfg["menu"]["db"]["entries"].".level, ".$cfg["menu"]["db"]["language"].".lang, ".$cfg["menu"]["db"]["language"].".label, ".$cfg["menu"]["db"]["language"].".exturl FROM ".$cfg["menu"]["db"]["entries"]." INNER JOIN ".$cfg["menu"]["db"]["language"]." ON ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["language"].".mid WHERE (((".$cfg["menu"]["db"]["entries"].".refid)=".$level1array["mid"].") AND ((".$cfg["menu"]["db"]["language"].".lang)='".$environment["language"]."')".$mandatory.") order by sort, label;";
-        #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level1sql: ".$sql.$debugging["char"];
+        //
+        $sql = "SELECT  ".$cfg["menu"]["db"]["entries"].".mid,
+                        ".$cfg["menu"]["db"]["entries"].".entry,
+                        ".$cfg["menu"]["db"]["entries"].".refid,
+                        ".$cfg["menu"]["db"]["entries"].".sort,
+                        ".$cfg["menu"]["db"]["entries"].".level,
+                        ".$cfg["menu"]["db"]["language"].".lang,
+                        ".$cfg["menu"]["db"]["language"].".label,
+                        ".$cfg["menu"]["db"]["language"].".exturl
+                  FROM  ".$cfg["menu"]["db"]["entries"]."
+            INNER JOIN  ".$cfg["menu"]["db"]["language"]."
+                    ON  ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["language"].".mid
+                 WHERE (
+                       (".$cfg["menu"]["db"]["entries"].".refid=".$level1array["mid"].")
+                   AND (".$cfg["menu"]["db"]["entries"].".hide <> '-1' OR ".$cfg["menu"]["db"]["entries"].".hide IS NULL)
+                   AND (".$cfg["menu"]["db"]["language"].".lang='".$environment["language"]."')
+                   ".$mandatory."
+                       )
+              ORDER BY sort, label;";
         $level2result = $db -> query($sql);
-        #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level1res: ".$submenuresult.$debugging["char"];
+        if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level2sql: ".$sql.$debugging["char"];
+        if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level2res: ".$level2result.$debugging["char"];
 
         // entweder alle in anderer sprache oder nichts
         #if ( $db -> num_rows($level1result) == 0 ){
@@ -131,30 +187,64 @@
                     }
                 }
                 if ( $right == -1 ) {
+                    // die boese schneide ab funktion
+                    if ( strlen($level2array["label"]) > $cfg["menu"]["level2"]["length"] ) {
+                        $level2array["label"] = substr($level2array["label"],0,$cfg["menu"]["level2"]["length"]-4)." ...";
+                    }
+                    // wo geht der href hin?
                     if ( $level2array["exturl"] == "" ) {
-                        // mandatory
+                        $href = $cfg["menu"]["base"]."/".$level1array["entry"]."/".$level2array["entry"].".html";
+                        $target = "";
+                    } else {
+                        $href = $level2array["exturl"];
+                        $target = $cfg["menu"]["level2"]["target"];
+                    }
+                    $marken = array("##target##", "##link##", "##label##");
+                    $ersatz = array($target, $href, $level2array["label"]);
+
+
+                    if ( $level2array["exturl"] == "" ) {
                         $mandatory = " AND ((".$cfg["menu"]["db"]["entries"].".mandatory)='-1')";
                         if ( $cfg["menu"]["level1"]["force"] == -1 ) $mandatory = "";
                         if ( strstr($environment["ebene"],"/".$level1array["entry"]."/".$level2array["entry"]) || strstr($environment["kategorie"],$level2array["entry"]) ) {
-                            // mandatory
                             if ( $cfg["menu"]["level3"]["full"] == -1 ) $mandatory = "";
                             if ( $cfg["menu"]["level3"]["dynamic"] == -1 ) $cfg["menu"]["level3"]["enable"] = -1;
                         } else {
                             if ( $cfg["menu"]["level3"]["dynamic"] == -1 ) $cfg["menu"]["level3"]["enable"] = 0;
                         }
-                       $ausgaben["punkte"] .= "<a class=\"".$cfg["menu"]["level2"]["style"]."\" href=\"".$pathvars["virtual"]."/".$level1array["entry"]."/".$level2array["entry"].".html\">".$level2array["label"]."</a><br>";
-                    } else {
-                       $ausgaben["punkte"] .= "<a class=\"".$cfg["menu"]["level2"]["style"]."\" target=\"_blank\" href=\"".$level2array["exturl"]."\">".$level2array["label"]."</a><br>";
-                    }
+                    #   $ausgaben["punkte"] .= "<a class=\"".$cfg["menu"]["level2"]["style"]."\" href=\"".$cfg["menu"]["base"]."/".$level1array["entry"]."/".$level2array["entry"].".html\">".$level2array["label"]."</a><br>";
+                    }# else {
+                    #   $ausgaben["punkte"] .= "<a class=\"".$cfg["menu"]["level2"]["style"]."\" target=\"_blank\" href=\"".$level2array["exturl"]."\">".$level2array["label"]."</a><br>";
+                    $ausgaben["punkte"] .= str_replace($marken,$ersatz,$cfg["menu"]["level2"]["link"]);
+                    #}
                 }
             }
 
+            //
             // menupunkte level 3
+            //
             if ( strstr($environment["ebene"],"/".$level1array["entry"]) || strstr($environment["kategorie"],$level1array["entry"]) ) {
-                $sql = "SELECT ".$cfg["menu"]["db"]["entries"].".mid, ".$cfg["menu"]["db"]["entries"].".entry, ".$cfg["menu"]["db"]["entries"].".refid, ".$cfg["menu"]["db"]["entries"].".sort, ".$cfg["menu"]["db"]["entries"].".level, ".$cfg["menu"]["db"]["language"].".lang, ".$cfg["menu"]["db"]["language"].".label, ".$cfg["menu"]["db"]["language"].".exturl FROM ".$cfg["menu"]["db"]["entries"]." INNER JOIN ".$cfg["menu"]["db"]["language"]." ON ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["language"].".mid WHERE (((".$cfg["menu"]["db"]["entries"].".refid)=".$level2array["mid"].") AND ((".$cfg["menu"]["db"]["language"].".lang)='".$environment["language"]."')".$mandatory.") order by sort, label;";
-                #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level2sql: ".$sql.$debugging["char"];
+                $sql = "SELECT  ".$cfg["menu"]["db"]["entries"].".mid,
+                                ".$cfg["menu"]["db"]["entries"].".entry,
+                                ".$cfg["menu"]["db"]["entries"].".refid,
+                                ".$cfg["menu"]["db"]["entries"].".sort,
+                                ".$cfg["menu"]["db"]["entries"].".level,
+                                ".$cfg["menu"]["db"]["language"].".lang,
+                                ".$cfg["menu"]["db"]["language"].".label,
+                                ".$cfg["menu"]["db"]["language"].".exturl
+                          FROM  ".$cfg["menu"]["db"]["entries"]."
+                    INNER JOIN  ".$cfg["menu"]["db"]["language"]."
+                            ON  ".$cfg["menu"]["db"]["entries"].".mid = ".$cfg["menu"]["db"]["language"].".mid
+                         WHERE (
+                               (".$cfg["menu"]["db"]["entries"].".refid=".$level2array["mid"].")
+                           AND (".$cfg["menu"]["db"]["entries"].".hide <> '-1' OR ".$cfg["menu"]["db"]["entries"].".hide IS NULL)
+                           AND (".$cfg["menu"]["db"]["language"].".lang='".$environment["language"]."')
+                           ".$mandatory."
+                               )
+                      ORDER BY  sort, label;";
                 $level3result = $db -> query($sql);
-                #if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "level2res: ".$submenuresult.$debugging["char"];
+                if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level3sql: ".$sql.$debugging["char"];
+                if ( $cfg["menu"]["db"]["debug"] ) $debugging["ausgabe"] .= "level3res: ".$level3result.$debugging["char"];
 
                 #if ( $db -> num_rows($level1result) == 0 ){
                 #    if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "Language: \"".$environment["language"]."\" for submenu not found using default: \"".$specialvars["default_language"]."\"".$debugging["char"];
@@ -174,11 +264,28 @@
                             }
                         }
                         if ( $right == -1 ) {
-                            if ( $level3array["exturl"] == "" ) {
-                               $ausgaben["punkte"] .= "<img src=\"../../images/".$environment["design"]."/menu.png\" width=\"2\" height=\"12\" align=\"bottom\"> <a class=\"".$cfg["menu"]["level3"]["style"]."\" href=\"".$pathvars["virtual"]."/".$level1array["entry"]."/".$level2array["entry"]."/".$level3array["entry"].".html\">".$level3array["label"]."</a><br>";
-                            } else {
-                               $ausgaben["punkte"] .= "<img src=\"../../images/".$environment["design"]."/menu.png\" width=\"2\" height=\"12\" align=\"bottom\"> <a class=\"".$cfg["menu"]["level3"]["style"]."\" target=\"_blank\" href=\"".$level3array["exturl"]."\">".$level3array["label"]."</a><br>";
+                            // die boese schneide ab funktion
+                            if ( strlen($level3array["label"]) > $cfg["menu"]["level3"]["length"] ) {
+                                $level3array["label"] = substr($level3array["label"],0,$cfg["menu"]["level3"]["length"]-4)." ...";
                             }
+                            // wo geht der href hin?
+                            if ( $level3array["exturl"] == "" ) {
+                                $href = $cfg["menu"]["base"]."/".$level1array["entry"]."/".$level2array["entry"]."/".$level3array["entry"].".html";
+                                $target = "";
+                            } else {
+                                $href = $level3array["exturl"];
+                                $target = $cfg["menu"]["level3"]["target"];
+                            }
+                            $marken = array("##target##", "##link##", "##label##");
+                            $ersatz = array($target, $href, $level3array["label"]);
+
+
+                            #if ( $level3array["exturl"] == "" ) {
+                            #   $ausgaben["punkte"] .= "<img src=\"../../images/".$environment["design"]."/menu.png\" width=\"2\" height=\"12\" align=\"bottom\"> <a class=\"".$cfg["menu"]["level3"]["style"]."\" href=\"".$cfg["menu"]["base"]."/".$level1array["entry"]."/".$level2array["entry"]."/".$level3array["entry"].".html\">".$level3array["label"]."</a><br>";
+                            #} else {
+                            #   $ausgaben["punkte"] .= "<img src=\"../../images/".$environment["design"]."/menu.png\" width=\"2\" height=\"12\" align=\"bottom\"> <a class=\"".$cfg["menu"]["level3"]["style"]."\" target=\"_blank\" href=\"".$level3array["exturl"]."\">".$level3array["label"]."</a><br>";
+                            #}
+                            $ausgaben["punkte"] .= str_replace($marken,$ersatz,$cfg["menu"]["level3"]["link"]);
                         }
                     }
                 }
