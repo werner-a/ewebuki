deb9d0c4a6535bcc31ae0a6f4e472494b41a8475
werner-a <chaot@chaos.de>
2006-07-31 10:16:38 +0000

    neuer viewer mit bilderstrecken funktion

--- basement/modules/basic/view.inc.php
+++ basement/modules/basic/view.inc.php
@@ -1,11 +1,11 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   $script["name"] = "$Id$";
-  $Script["desc"] = "short description";
+  $Script["desc"] = "picture viewer";
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
     eWeBuKi - a easy website building kit
-    Copyright (C)2001, 2002, 2003 Werner Ammon <wa@chaos.de>
+    Copyright (C)2001-2006 Werner Ammon ( wa<at>chaos.de )
 
     This script is a part of eWeBuKi
 
@@ -45,24 +45,154 @@
 
     if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ** ".$script["name"]." ** ]".$debugging["char"];
 
-    $ausgaben["output"] = "test";
+        // page basics
+        // ***
 
-    /*
-    if ( strstr($imgwerte[0], $pathvars["filebase"]["webdir"]) ) {
-        $imgfile = str_replace($pathvars["filebase"]["webdir"],"",$imgwerte[0]);
-        $imgfile = $pathvars["filebase"]["maindir"].$imgfile;
-    } else {
-        $imgfile = $pathvars["fileroot"].$imgwerte[0];
-    }
-    if ( file_exists($imgfile) ) {
-        $imgsize = getimagesize($imgfile);
-        $imgsize = " ".$imgsize[3];
-    }
-    */
+        // warnung ausgeben
+        if ( get_cfg_var('register_globals') == 1 ) $debugging["ausgabe"] .= "Warnung: register_globals in der php.ini steht auf on, evtl werden interne Variablen ueberschrieben!".$debugging["char"];
 
-    $imgurl = $pathvars["webroot"].$pathvars["filebase"]["webdir"].$pathvars["filebase"]["pic"]["root"].$pathvars["filebase"]["pic"][$environment["parameter"][1]].$environment["parameter"][2];
-    $ausgaben["output"] = "<a href=\"".$_SERVER["HTTP_REFERER"]."\"><img border=\"0\" src=\"".$imgurl."\" alt=\"".$beschriftung."\"".$align.$border.$imgsize." /></a>";
-    $ausgaben["output"] .= "<br /><br /><a href=\"".$_SERVER["HTTP_REFERER"]."\">Zurück</a>";
+        // path fuer die schaltflaechen anpassen
+        if ( $cfg["iconpath"] == "" ) $cfg["iconpath"] = "/images/default/";
+
+        // label bearbeitung aktivieren
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $specialvars["editlock"] = 0;
+        } else {
+            $specialvars["editlock"] = -1;
+        }
+
+        // +++
+        // page basics
+
+
+        // funktions bereich
+        // ***
+
+        $fid = $environment["parameter"][2];
+
+        if ( $environment["parameter"][3] != "" ) {
+            $sql = "SELECT *
+                    FROM ".$cfg["db"]["entries"]."
+                    WHERE fhit like '#p".$environment["parameter"][3]."%'
+                ORDER BY ".$cfg["db"]["order"];
+        } else {
+            $sql = "SELECT *
+                    FROM ".$cfg["db"]["entries"]."
+                    WHERE fid =".$fid;
+        }
+        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+        $result = $db -> query($sql);
+
+
+
+        $erste = "";
+        while ( $data = $db -> fetch_array($result,1) ) {
+            $dataloop["leer"][$data["fid"]][0] = $data["fid"];
+            $dataloop["leer"][$data["fid"]][1] = $data["fhit"];
+
+            $gesamt++;
+
+            if ( $erste == "" ) $erste = $data["fid"];
+
+            if ( $gefunden == True ) {
+                $nachher = $data["fid"];
+                $gefunden = False;
+            }
+
+            if ( $data["fid"] == $fid ) {
+                $gefunden = True;
+                $vorher = $merker;
+                $aktuell = $gesamt;
+                $filename = $data["ffname"];
+                $filetyp = $data["ffart"];
+                $ausgaben["beschriftung"] = $data["funder"];
+                $ausgaben["beschreibung"] = $data["fdesc"];
+            }
+
+            $merker = $data["fid"];
+
+            #$marke = substr($data["fhit"],0,strpos($data["fhit"],"#",1));
+            #$merke = str_replace( "#p".$environment["parameter"][3].",", "", $marke )."<br />";
+        }
+        $letzte = $merker;
+        #$hidedata["leer"][0] = "enable";
+
+        // ueberlauf sicherstellen
+        if ( $fid == $erste ) {
+            $vorher = $letzte;
+        }
+        if ( $fid == $letzte ) {
+            $nachher = $erste;
+        }
+
+        // navi links
+        $ausgaben["zurueck"] = "view,".$environment["parameter"][1].",".$vorher.",".$environment["parameter"][3].".html";
+        $ausgaben["aktuell"] = $aktuell;
+        $ausgaben["gesamt"] = $gesamt;
+        $ausgaben["vor"] = "view,".$environment["parameter"][1].",".$nachher.",".$environment["parameter"][3].".html";
+        $ausgaben["referer"] = dirname($pathvars["requested"]).".html";
+        if ( $environment["parameter"][3] != "" ) {
+            $hidedata["navi"][0] = "enable";
+        } else {
+            $ausgaben["vor"] = $ausgaben["referer"];
+        }
+
+
+        // img werte
+        if ( $pathvars["filebase"]["realname"] == True ) {
+            $img = $filetyp."/".$fid."/".$environment["parameter"][1]."/".$filename;
+        } else {
+            $img =  $pathvars["filebase"]["pic"]["root"].$pathvars["filebase"]["pic"][$environment["parameter"][1]]."img_".$fid.".".$filetyp;
+        }
+
+        $imgfile = $pathvars["filebase"]["maindir"].$img;
+        $ausgaben["imgurl"] = $pathvars["webroot"].$pathvars["filebase"]["webdir"].$img;
+        if ( file_exists($imgfile) ) {
+            $imgsize = getimagesize($imgfile);
+            $ausgaben["imgsize"] = " ".$imgsize[3];
+        }
+
+        // +++
+        // funktions bereich
+
+
+        // page basics
+        // ***
+
+        // fehlermeldungen
+        if ( $HTTP_GET_VARS["error"] != "" ) {
+            if ( $HTTP_GET_VARS["error"] == 1 ) {
+                $ausgaben["form_error"] = "#(error1)";
+            }
+        } else {
+            $ausgaben["form_error"] = "";
+        }
+
+        // navigation erstellen
+        #$ausgaben["add"] = $cfg["basis"]."/add,".$environment["parameter"][1].",verify.html";
+        #$mapping["navi"] = "leer";
+
+        // hidden values
+        #$ausgaben["form_hidden"] .= "";
+
+        // was anzeigen
+        #$mapping["main"] = crc32($environment["ebene"]).".list";
+        $mapping["main"] = "viewer";
+        #$mapping["navi"] = "leer";
+
+        // unzugaengliche #(marken) sichtbar machen
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $ausgaben["inaccessible"] = "inaccessible values:<br />";
+            $ausgaben["inaccessible"] .= "# (error1) #(error1)<br />";
+        } else {
+            $ausgaben["inaccessible"] = "";
+        }
+
+        // wohin schicken
+        #n/a
+
+        // +++
+        // page basics
 
     if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ++ ".$script["name"]." ++ ]".$debugging["char"];
 
