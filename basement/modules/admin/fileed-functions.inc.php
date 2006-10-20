c4565a3234c8c146b73cb907e20010f9ff87bef8
werner-a <chaot@chaos.de>
2006-10-20 20:51:12 +0000

    version 1.3 wiederhergestelt

--- basement/modules/admin/fileed-functions.inc.php
+++ basement/modules/admin/fileed-functions.inc.php
@@ -159,9 +159,6 @@
                 case "jpg":
                     imagejpeg($img_dst,$img_path."/".$img_name."_".$img_id.".jpg");
                     break;
-                case "jpeg":
-                    imagejpeg($img_dst,$img_path."/".$img_name."_".$img_id.".jpeg");
-                    break;
                 case "png":
                     imagepng($img_dst,$img_path."/".$img_name."_".$img_id.".png");
                     break;
@@ -190,7 +187,7 @@
                     case "gif":
                         $img_src = @imagecreatefromgif($source);
                         break;
-                    case "jpg": case "jpeg":
+                    case "jpg":
                         $img_src = @imagecreatefromjpeg($source);
                         break;
                     case "png":
