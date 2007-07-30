7a732fdc0ad9123b502ec30c06db58304c18bdda
werner-a <chaot@chaos.de>
2007-07-30 13:39:36 +0000

    preview search funktion

---
+++ basement/modules/addon/search.inc.php
@@ -0,0 +1,157 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  $script["name"] = "$Id: search.inc.php 503 2006-09-22 06:16:23Z chaot $";
+  $Script["desc"] = "suche im content";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001-2006 Werner Ammon ( wa<at>chaos.de )
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ** ".$script["name"]." ** ]".$debugging["char"];
+
+    if ( $cfg["right"] == "" || $rechte[$cfg["right"]] == -1 ) {
+
+        ////////////////////////////////////////////////////////////////////
+        // achtung: bei globalen funktionen, variablen nicht zuruecksetzen!
+        // z.B. $ausgaben["form_error"],$ausgaben["inaccessible"]
+        ////////////////////////////////////////////////////////////////////
+
+        // page basics
+        // ***
+
+        // warnung ausgeben
+        if ( get_cfg_var('register_globals') == 1 ) $debugging["ausgabe"] .= "Warnung: register_globals in der php.ini steht auf on, evtl werden interne Variablen ueberschrieben!".$debugging["char"];
+
+        // path fuer die schaltflaechen anpassen
+        if ( $cfg["iconpath"] == "" ) $cfg["iconpath"] = "/images/default/";
+
+        // label bearbeitung aktivieren
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $specialvars["editlock"] = 0;
+        } else {
+            $specialvars["editlock"] = -1;
+        }
+
+        // +++
+        // page basics
+
+
+        // funktions bereich
+        // ***
+
+        $ausgaben["search"] = $HTTP_POST_VARS["search"];
+
+        if ( $HTTP_POST_VARS["search"] != "" ) {
+            $sql = "SELECT *
+                    FROM ".$cfg["db"]["text"]["entries"]."
+                    WHERE ".$cfg["db"]["text"]["where"]." LIKE '%".$HTTP_POST_VARS["search"]."%'";;
+            if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+            $result = $db -> query($sql);
+            $hits = $db -> num_rows($result);
+
+            if ( $hits == 0 ) {
+                $ausgaben["result"] = "#(found_nothing)";
+            } elseif ( $hits == 1 ) {
+                $data = $db -> fetch_array($result,1);
+                header("Location: ".$pathvars["virtual"].$data["ebene"]."/".$data["kategorie"].".html");
+            } else {
+                $ausgaben["result"] = "#(found_something)";
+                // [H1]Treffer Ihrer Suche nach "!#ausgaben_search"[/H1]
+                while ( $data = $db -> fetch_array($result,1) ) {
+                    #$dataloop["leer"][$data["id"]][1] = $data["field1"];
+                    $dataloop["result"][$data["label"]."_".$data["tname"]]["found_in"] = $data["ebene"].$data["kategorie"].".html";
+                    $dataloop["result"][$data["label"]."_".$data["tname"]]["content"] = $data["content"];
+                }
+                $hidedata["result"][0] = "enable";
+            }
+        } else {
+            header("Location: ".$HTTP_POST_VARS["requested"]);
+        }
+
+        // +++
+        // funktions bereich
+
+
+        // page basics
+        // ***
+
+        // fehlermeldungen
+        if ( $HTTP_GET_VARS["error"] != "" ) {
+            if ( $HTTP_GET_VARS["error"] == 1 ) {
+                $ausgaben["form_error"] = "#(error1)";
+            }
+        } else {
+            $ausgaben["form_error"] = "";
+        }
+
+        // navigation erstellen
+        #$ausgaben["add"] = $cfg["basis"]."/add,".$environment["parameter"][1].",verify.html";
+        #$mapping["navi"] = "leer";
+
+        // hidden values
+        #$ausgaben["form_hidden"] .= "";
+
+        // was anzeigen
+        #$mapping["main"] = crc32($environment["ebene"]).".list";
+        #$mapping["navi"] = "leer";
+
+        // unzugaengliche #(marken) sichtbar machen
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $ausgaben["inaccessible"] = "inaccessible values:<br />";
+            $ausgaben["inaccessible"] .= "# (found_nothing) #(found_nothing)<br />";
+            $ausgaben["inaccessible"] .= "# (found_something) #(found_something)<br />";
+            $ausgaben["inaccessible"] .= "# (error1) #(error1)<br />";
+        } else {
+            $ausgaben["inaccessible"] = "";
+        }
+
+        // wohin schicken
+        #n/a
+
+        // +++
+        // page basics
+
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+    if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ++ ".$script["name"]." ++ ]".$debugging["char"];
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
