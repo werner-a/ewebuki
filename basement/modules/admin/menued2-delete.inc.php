da7cd183b936060c5407bd7d6765633bf8974870
EmNiLi <buffy@level14.de>
2007-09-17 09:30:06 +0000

    erst mal eine kopie des menu-editors

---
+++ basement/modules/admin/menued2-delete.inc.php
@@ -0,0 +1,214 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: menued-delete.inc.php 749 2007-09-13 13:40:10Z buffy1860 $";
+// "short description";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001, 2002, 2003 Werner Ammon <wa@chaos.de>
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #} elseif ( $environment["parameter"][1] == "delete" && $rechte[$cfg["right"]] == -1 ) {
+    if ( $rechte[$cfg["right"]] == -1 ) {
+
+        // erst mal kucken ob leoschen eine gute idee ist?
+        // ***
+        $sql = "SELECT ".$cfg["db"]["menu"]["key"]."
+                  FROM ".$cfg["db"]["menu"]["entries"]."
+                 WHERE refid='".$environment["parameter"][1]."'";
+        $result = $db -> query($sql);
+        $num_rows = $db -> num_rows($result);
+        // +++
+        // erst mal kucken ob leoschen eine gute idee ist?
+
+        if ( $num_rows > 0 ) {
+
+            // was anzeigen
+            $mapping["main"] = crc32($environment["ebene"]).".list";
+            $mapping["navi"] = "leer";
+
+            // wohin schicken
+            header("Location: ".$cfg["basis"]."/list.html?error=1");
+
+        } else {
+
+            // menupunkt holen
+            // ***
+            $sql = "SELECT mid, refid, entry
+                      FROM ".$cfg["db"]["menu"]["entries"]."
+                     WHERE ".$cfg["db"]["menu"]["key"]."='".$environment["parameter"][1]."'";
+            $result = $db -> query($sql);
+            $array = $db -> fetch_array($result,$nop);
+            $refid = $array["refid"];
+            $kategorie = $array["entry"];
+            $ausgaben["entry"] = $kategorie;
+            $ausgaben["form_hidden"] .= "<input type=\"hidden\" name=\"mid\" value=\"".$array["mid"]."\" class=\"hidden\"/>";
+            // +++
+            // menupunkt holen
+
+            // bezeichnungen holen (alle sprachen)
+            // ***
+            $sql = "SELECT mlid, lang, label
+                      FROM ".$cfg["db"]["lang"]["entries"]."
+                     WHERE mid='".$environment["parameter"][1]."'";
+            $result = $db -> query($sql);
+            while ( $array = $db -> fetch_array($result,$nop) ) {
+                if ( $mlids != "" ) $mlids .= ",";
+                $mlids .= $array["mlid"];
+                $ausgaben["languages"] .= $array["lang"]." ";
+                $ausgaben["languages"] .= $array["label"]."<br />";
+            }
+            $ausgaben["form_hidden"] .= "<input type=\"hidden\" name=\"mlids\" value=\"".$mlids."\" class=\"hidden\"/>";
+            // +++
+            // bezeichnungen holen (alle sprachen)
+
+
+            // content holen (alle sprachen)
+            // ***
+            $ebene = make_ebene($refid);
+            if ( $ebene != "/" ) $extend = crc32($ebene).".";
+            $tname = $extend.$kategorie;
+            $sql = "SELECT lang, label, tname, content
+                      FROM ".$cfg["db"]["text"]["entries"]."
+                     WHERE tname='".$tname."';";
+            $result  = $db -> query($sql);
+            while ( $array = $db -> fetch_array($result,$nop) ) {
+                $ausgaben["content"] .=  $array["lang"].": "
+                                        .$array["label"].": "
+                                        .substr($array["content"],0,20)." ...<br />";
+            }
+            if ( $ausgaben["content"] == "" ) $ausgaben["content"] = "#(no_content)";
+            $ausgaben["form_hidden"] .= "<input type=\"hidden\" name=\"tname\" value=\"".$tname."\" class=\"hidden\"/>";
+            // +++
+            // content holen (alle sprachen)
+
+
+            // page basics
+            // ***
+            // fehlermeldungen
+            $ausgaben["form_error"] = "";
+
+            // navigation erstellen
+            $ausgaben["form_aktion"] = $cfg["basis"]."/delete,".$environment["parameter"][1].".html";
+            $ausgaben["form_break"] = $cfg["basis"]."/list.html";
+
+            // hidden values
+            $ausgaben["form_hidden"] .= "<input type=\"hidden\" name=\"delete\" value=\"true\" class=\"hidden\"/>";
+
+            // was anzeigen
+            $mapping["main"] = crc32($environment["ebene"]).".delete";
+            $mapping["navi"] = "leer";
+
+            // unzugaengliche #(marken) sichtbar machen
+            // ***
+            if ( isset($HTTP_GET_VARS["edit"]) ) {
+                $ausgaben["inaccessible"] = "inaccessible values:<br />";
+                $ausgaben["inaccessible"] .= "# (error_menu) #(error_menu)<br />";
+                $ausgaben["inaccessible"] .= "# (error_menu_lang) #(error_menu_lang)<br />";
+                $ausgaben["inaccessible"] .= "# (error_text) #(error_text)<br />";
+                $ausgaben["inaccessible"] .= "# (no_content) #(no_content)<br />";
+            } else {
+                $ausgaben["inaccessible"] = "";
+            }
+            // +++
+            // unzugaengliche #(marken) sichtbar machen
+
+            // wohin schicken
+            # header("Location: ".$cfg["basis"]."/?.html");
+            // +++
+            // page basics
+
+
+            // das loeschen wurde bestaetigt, loeschen!
+            // ***
+            if ( $HTTP_POST_VARS["delete"] ) {
+                unset($result);
+
+                // content loeschen
+                // ***
+                if ( $HTTP_POST_VARS["tname"] != "" ) {
+                    $sql = "DELETE FROM ".$cfg["db"]["text"]["entries"]." WHERE tname = '".$HTTP_POST_VARS["tname"]."'";
+                    if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                    $result  = $db -> query($sql);
+                    if ( !$result ) $ausgaben["form_error"] = $db -> error("#(text_error)<br />");
+                }
+                // +++
+                // content loeschen
+
+                // ohne fehler bezeichnungen loeschen
+                // ***
+                if ( $HTTP_POST_VARS["mlids"] != "" && $ausgaben["form_error"] == "" ) {
+                    $array = split(",",$HTTP_POST_VARS["mlids"]);
+                    foreach( $array as $value) {
+                        $sql = "DELETE FROM ".$cfg["db"]["lang"]["entries"]." WHERE ".$cfg["db"]["lang"]["key"]."='".$value."';";
+                        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                        $result  = $db -> query($sql);
+                        if ( !$result ) $ausgaben["form_error"] = $db -> error("#(menu_lang_error)<br />");
+                    }
+                }
+                // +++
+                // ohne fehler bezeichnungen loeschen
+
+                // ohne fehler menupunkte loeschen
+                // ***
+                if ( $HTTP_POST_VARS["mid"] != "" && $ausgaben["form_error"] == "" ) {
+                    $array = split(",",$HTTP_POST_VARS["mid"]);
+                    foreach( $array as $value) {
+                        $sql = "DELETE FROM ".$cfg["db"]["menu"]["entries"]." WHERE ".$cfg["db"]["menu"]["key"]."='".$value."';";
+                        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                        $result  = $db -> query($sql);
+                        if ( !$result ) $ausgaben["form_error"] = $db -> error("#(menu_error)<br />");
+                    }
+                }
+                // +++
+                // ohne fehler menupunkte loeschen
+
+                // wohin schicken
+                if ( $ausgaben["form_error"] == "" ) {
+                    header("Location: ".$cfg["basis"]."/list.html");
+                }
+            }
+            // +++
+            // das loeschen wurde bestaetigt, loeschen!
+        }
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
---
+++ basement/modules/admin/menued2-move.inc.php
@@ -0,0 +1,197 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: menued-move.inc.php 397 2006-03-24 13:42:35Z chaot $";
+// "short description";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001, 2002, 2003 Werner Ammon <wa@chaos.de>
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #} elseif ( $environment["parameter"][1] == "move" && $rechte[$cfg["right"]] == -1 ) {
+    if ( $rechte[$cfg["right"]] == -1 ) {
+
+        // page basics
+        // ***
+        if ( count($HTTP_POST_VARS) == 0 ) {
+            $sql = "SELECT * FROM ".$cfg["db"]["menu"]["entries"]." WHERE ".$cfg["db"]["menu"]["key"]."='".$environment["parameter"][1]."'";            $result = $db -> query($sql);
+            $form_values = $db -> fetch_array($result,1);
+        } else {
+            $form_values = $HTTP_POST_VARS;
+        }
+
+        // form options holen
+        $form_options = form_options(crc32($environment["ebene"]).".".$environment["kategorie"]);
+
+        // form elememte bauen
+        $element = form_elements( $cfg["db"]["menu"]["entries"], $form_values );
+
+        // form elemente erweitern
+        #$element["new_lang"] = "<input name=\"new_lang\" type=\"text\" maxlength=\"5\" size=\"5\">";
+        // +++
+        // page basics
+
+
+        $ausgaben["output"] .= sitemap(0, "select", $environment["parameter"][1]);
+
+
+        // page basics
+        // ***
+        // fehlermeldungen
+        $ausgaben["form_error"] = "";
+
+        // navigation erstellen
+        $ausgaben["form_aktion"] = $cfg["basis"]."/move,".$environment["parameter"][1].",verify.html";
+        $ausgaben["form_break"] = $cfg["basis"]."/list.html";
+
+        // hidden values
+        $ausgaben["form_hidden"] .= "";
+
+        // was anzeigen
+        $mapping["main"] = crc32($environment["ebene"]).".move";
+        $mapping["navi"] = "leer";
+
+        // unzugaengliche #(marken) sichtbar machen
+        // ***
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $ausgaben["inaccessible"] = "inaccessible values:<br />";
+            $ausgaben["inaccessible"] .= "# (error_dupe) #(error_dupe)<br />";
+        } else {
+            $ausgaben["inaccessible"] = "";
+        }
+        // +++
+        // unzugaengliche #(marken) sichtbar machen
+
+        // wohin schicken
+        # header("Location: ".$cfg["basis"]."/?.html");
+        // +++
+        // page basics
+
+        if ( $environment["parameter"][2] == "verify"
+            && $HTTP_POST_VARS["send"] != "" ) {
+
+            // form eigaben prüfen
+            form_errors( $form_options, $HTTP_POST_VARS );
+
+            // gibt es in der neuen ebene einen solchen entry?
+            $sql = "SELECT entry
+                      FROM ".$cfg["db"]["menu"]["entries"]."
+                     WHERE refid = '".$HTTP_POST_VARS["refid"]."'
+                       AND entry = '".$HTTP_POST_VARS["entry"]."'";
+            $result = $db -> query($sql);
+            #$data = $db -> fetch_array($result,1);
+            $num_rows = $db -> num_rows($result);
+            if ( $num_rows >= 1 ) $ausgaben["form_error"] .= "#(error_dupe)";
+
+            // content tabellen aenderungen
+            if ( $ausgaben["form_error"] == "" ) {
+                $sql = "SELECT refid, entry FROM ".$cfg["db"]["menu"]["entries"]." WHERE ".$cfg["db"]["menu"]["key"]."='".$environment["parameter"][1]."'";
+                $result = $db -> query($sql);
+                $data = $db -> fetch_array($result,1);
+
+                // content aktuelle seite aendern (alle sprachen)
+                $ebene = make_ebene($data["refid"]);
+                if ( $ebene != "/" ) {
+                    $extend = crc32($ebene).".";
+                } else {
+                    $ebene = "";
+                    $extend = "";
+                }
+                $old_tname = $extend.$data["entry"];
+                #echo $ebene.":".$old_tname."<br>";
+                $suchmuster = $ebene."/".$data["entry"];
+
+                $ebene = make_ebene($HTTP_POST_VARS["refid"]);
+                if ( $ebene != "/" ) {
+                    $extend = crc32($ebene).".";
+                } else {
+                    $ebene = "";
+                    $extend = "";
+                }
+                $new_tname = $extend.$HTTP_POST_VARS["entry"];
+                #echo $ebene.":".$new_tname."<br>";
+                $ersatz = $ebene."/".$HTTP_POST_VARS["entry"];
+
+                $sql = "UPDATE ".$cfg["db"]["text"]["entries"]."
+                            SET tname = '".$new_tname."',
+                                ebene = '".$ebene."',
+                                kategorie = '".$HTTP_POST_VARS["entry"]."'
+                            WHERE tname = '".$old_tname."';";
+                if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                $result  = $db -> query($sql);
+                if ( !$result ) $ausgaben["form_error"] .= $db -> error("#(menu_error)<br />");
+
+                // content der unterpunkte aendern (alle sprachen)
+                update_tname($environment["parameter"][1], $suchmuster, $ersatz);
+            }
+
+
+            // menu tabellen aenderungen
+            if ( $ausgaben["form_error"] == "" ) {
+                $kick = array( "PHPSESSID", "send", "image", "image_x", "image_y", "form_referer",
+                               "entry" );
+                foreach($HTTP_POST_VARS as $name => $value) {
+                    if ( !in_array($name,$kick) && !strstr($name, ")" ) ) {
+                        if ( $sqla != "" ) $sqla .= ", ";
+                        $sqla .= $name."='".$value."'";
+                    }
+                }
+
+                // Sql um spezielle Felder erweitern
+                #$entry = strtolower($HTTP_POST_VARS["entry"]);
+                #$entry = str_replace(" ", "", $entry);
+                #$sqla .= ", entry='".$entry."'";
+
+                $sql = "update ".$cfg["db"]["menu"]["entries"]." SET ".$sqla." WHERE ".$cfg["db"]["menu"]["key"]."='".$environment["parameter"][1]."'";
+                if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                $result  = $db -> query($sql);
+                if ( !$result ) $ausgaben["form_error"] .= $db -> error("#(menu_error)<br />");
+                if ( $header == "" ) $header = $cfg["basis"]."/list.html";
+            }
+
+            // wenn es keine fehlermeldungen gab, die uri $header laden
+            if ( $ausgaben["form_error"] == "" ) {
+                header("Location: ".$header);
+            }
+        }
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
