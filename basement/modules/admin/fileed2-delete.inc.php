6b04926b60453d55e631df11451786f41771655b
Krompi <krompi@gmail.com>
2007-09-18 11:47:05 +0000

    initial fuer den neuen fileed

---
+++ basement/modules/admin/fileed2-delete.inc.php
@@ -0,0 +1,243 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: fileed-delete.inc.php 523 2006-10-10 11:04:15Z chaot $";
+// "leer - delete funktion";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001-2006 Werner Ammon ( wa<at>chaos.de )
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    if ( $rechte[$cfg["right"]] == "" || $rechte[$cfg["right"]] == -1 ) {
+
+        // funktions bereich fuer erweiterungen
+        // ***
+
+        if ( count($_SESSION["file_memo"]) > 0 ) {
+            $environment["parameter"][1] = current($_SESSION["file_memo"]);
+        } else  {
+            header("Location: ".$cfg["basis"]."/list.html");
+            exit();
+        }
+
+        ### put your code here ###
+
+        // wird die datei im content verwendet?
+        $old = "\_".$environment["parameter"][1].".";
+        $new = "/".$environment["parameter"][1]."/";
+        #$new = "=".$pathvars["filebase"]["webdir"].$data["ffart"]."/".$data["fid"]."/";
+        $sql = "SELECT *
+                  FROM ".$cfg["db"]["content"]["entries"]."
+                 WHERE ".$cfg["db"]["content"]["content"]." LIKE '%".$old."%'
+                    OR ".$cfg["db"]["content"]["content"]." LIKE '%".$new."%'";
+        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+        $result = $db -> query($sql);
+        $num_rows = $db -> num_rows($result);
+
+        // +++
+        // funktions bereich fuer erweiterungen
+
+        if ( $num_rows > 0 ) {
+
+            // was anzeigen
+            $mapping["main"] = crc32($environment["ebene"]).".list";
+            $mapping["navi"] = "leer";
+
+            // wohin schicken
+            header("Location: ".$cfg["basis"]."/list.html?error=1");
+
+        } else {
+
+            // datensatz holen
+            $sql = "SELECT *
+                      FROM ".$cfg["db"]["file"]["entries"]."
+                     WHERE ".$cfg["db"]["file"]["key"]."='".$environment["parameter"][1]."'";
+            if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+            $result = $db -> query($sql);
+            $data = $db -> fetch_array($result,$nop);
+            $ausgaben["form_id1"] = $data["fid"];
+            #$ausgaben["field1"] = $data["field1"];
+            #$ausgaben["field2"] = $data["field2"];
+            $ausgaben["ffname"] = $data["ffname"];
+            $ausgaben["ffart"] = $data["ffart"];
+
+            // funktions bereich fuer erweiterungen
+            // ***
+
+            ### put your code here ###
+
+            if ( $_SESSION["uid"] != $data["fuid"] && in_array( $environment["kategorie"], $cfg["restrict"]) ) { # nur eigene dateien duerfen gelöscht werden
+                header("Location: ".$cfg["basis"]."/list.html?error=2");
+                exit();
+            }
+
+
+            /* z.B. evtl. verknuepfte datensatze holen
+            $sql = "SELECT *
+                      FROM ".$cfg["db"]["more"]["entries"]."
+                     WHERE ".$cfg["db"]["more"]["key"]." ='".$environment["parameter"][1]."'";
+            if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+            $result = $db -> query($sql);
+            while ( $data2 = $db -> fetch_array($result,$nop) ) {
+                if ( $ids != "" ) $ids .= ",";
+                $ids .= $array["id"];
+                $ausgaben["field3"] .= $array["field1"]." ";
+                $ausgaben["field3"] .= $array["field2"]."<br />";
+            }
+            $ausgaben["form_id2"] = $ids;
+            */
+
+            // +++
+            // funktions bereich fuer erweiterungen
+
+
+            // page basics
+            // ***
+
+            // fehlermeldungen
+            $ausgaben["form_error"] = "";
+
+            // navigation erstellen
+            $ausgaben["form_aktion"] = $cfg["basis"]."/delete,".$environment["parameter"][1].".html";
+            $ausgaben["form_break"] = $cfg["basis"]."/list.html";
+
+            // hidden values
+            $ausgaben["form_hidden"] = "";
+            $ausgaben["form_delete"] = "true";
+
+            // was anzeigen
+            #$mapping["main"] = crc32($environment["ebene"]).".delete";
+            #$mapping["navi"] = "leer";
+
+            // unzugaengliche #(marken) sichtbar machen
+            // ***
+            if ( isset($HTTP_GET_VARS["edit"]) ) {
+                $ausgaben["inaccessible"] = "inaccessible values:<br />";
+                $ausgaben["inaccessible"] .= "# (error_result1) #(error_result1)<br />";
+                $ausgaben["inaccessible"] .= "# (error_result2) #(error_result2)<br />";
+            } else {
+                $ausgaben["inaccessible"] = "";
+            }
+            // +++
+            // unzugaengliche #(marken) sichtbar machen
+
+            // wohin schicken
+            #n/a
+
+            // +++
+            // page basics
+
+
+            // das loeschen wurde bestaetigt, loeschen!
+            // ***
+            if ( $HTTP_POST_VARS["delete"] != ""
+                && $HTTP_POST_VARS["send"] != "" ) {
+
+                // evtl. zusaetzlichen datensatz loeschen
+                if ( $HTTP_POST_VARS["id2"] != "" ) {
+                    // funktions bereich fuer erweiterungen
+                    // ***
+
+                    ### put your code here ###
+
+                    /* z.B. evtl. verknuepfte datensatze loeschen
+                    $sql = "DELETE FROM ".$cfg["db"]["more"]["entries"]."
+                                  WHERE ".$cfg["db"]["more"]["key"]." = '".$HTTP_POST_VARS["id2"]."'";
+                    if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                    $result  = $db -> query($sql);
+                    if ( !$result ) $ausgaben["form_error"] = $db -> error("#(error_result2)<br />");
+                    */
+
+                    // +++
+                    // funktions bereich fuer erweiterungen
+                }
+
+                // datei loeschen
+                if ( $ausgaben["form_error"] == "" ) {
+
+                    $id = $HTTP_POST_VARS["id1"];
+
+                    $sql = "SELECT ffart, fuid FROM site_file WHERE fid =".$id;
+                    $result = $db -> query($sql);
+                    $data = $db -> fetch_array($result,$nop);
+
+                    $type = $cfg["filetyp"][$data["ffart"]];
+                    if ( $type == "img" ) {
+                        $art = array( "o" => "img", "s" => "img", "m" => "img", "b" => "img", "tn" => "tn" );
+                        foreach ( $art as $key => $value ) {
+                            $return = unlink($cfg["fileopt"][$type]["path"].$pathvars["filebase"]["pic"][$key].$value."_".$id.".".$data["ffart"]);
+                            ### sollte evtl. anderst gelöst werden, existiert nur ein file nicht
+                            ### laesst sich der datensatz nie löschen!
+                            if ( $return != 1 ) {
+                                #$ausgaben["form_error"] = "error delete files";
+                                break;
+                            }
+                        }
+                    } else {
+                        $return = unlink($cfg["fileopt"][$type]["path"].$cfg["fileopt"][$type]["name"]."_".$id.".".$data["ffart"]);
+                        if ( $return != "1" ) {
+                            $ausgaben["form_error"] = "error delete file";
+                        }
+                    }
+                    unset ($_SESSION["file_memo"][$id]);
+                }
+
+                // datensatz loeschen
+                if ( $ausgaben["form_error"] == "" ) {
+                    $sql = "DELETE FROM ".$cfg["db"]["file"]["entries"]."
+                                  WHERE ".$cfg["db"]["file"]["key"]."='".$HTTP_POST_VARS["id1"]."';";
+                    if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+                    $result  = $db -> query($sql);
+                    if ( !$result ) $ausgaben["form_error"] = $db -> error("#(error_result1)<br />");
+                }
+                // +++
+
+                // wohin schicken
+                if ( $ausgaben["form_error"] == "" ) {
+                    header("Location: ".$cfg["basis"]."/delete.html");
+                }
+            }
+            // +++
+            // das loeschen wurde bestaetigt, loeschen!
+        }
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
---
+++ basement/modules/admin/fileed2-list.inc.php
@@ -0,0 +1,338 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: fileed-list.inc.php 520 2006-10-06 19:10:05Z chaot $";
+// "fileed - list funktion";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001-2006 Werner Ammon ( wa<at>chaos.de )
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    if ( $cfg["right"] == "" || $rechte[$cfg["right"]] == -1 ) {
+
+        // funktions bereich ( aufbau )
+        // ***
+
+        // file_memo verwalten
+        if ( $environment["parameter"][2] ) {
+            $key = $environment["parameter"][2];
+            $wert = $environment["parameter"][2];
+            if ( is_array( $_SESSION["file_memo"] ) ) {
+                if ( in_array($key, $_SESSION["file_memo"] ) ) {
+                    unset ( $_SESSION["file_memo"][$key] );
+                } else {
+                    $_SESSION["file_memo"][$key] = $wert;
+                }
+            } else {
+                $_SESSION["file_memo"][$key] = $wert;
+            }
+        }
+        $debugging["ausgabe"] .= "<pre>".print_r($_SESSION["file_memo"],True)."</pre>";
+
+        // auswahllisten erstellen
+        $set = array(); $data = array();
+        $_SESSION["fileed_filter0"] = $_SESSION["fileed_filter0"] + 0;
+        $_SESSION["fileed_filter1"] = $_SESSION["fileed_filter1"] + 0;
+        foreach( $cfg["filter"] as $set => $data ) {
+            if ( $HTTP_GET_VARS["filter".$set] != "" ) {
+                $_SESSION["fileed_filter".$set] = $HTTP_GET_VARS["filter".$set];
+            }
+            $dataloop["filter".$set][$_SESSION["fileed_filter".$set]]["select"] =  " selected";
+            foreach ( $data as $key => $value ) {
+                $dataloop["filter".$set][$key]["value"] = $key;
+                $dataloop["filter".$set][$key]["label"] = $value;
+            }
+            $debugging["ausgabe"] .= "<pre>".print_r($dataloop["filter".$set],True)."</pre>";
+        }
+
+        // content editor link erstellen
+        if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "SESSION (cms_last_edit): ".$_SESSION["cms_last_edit"].$debugging["char"];
+        if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "SESSION (cms_last_referer): ".$_SESSION["cms_last_referer"].$debugging["char"];
+        if ( isset($_SESSION["cms_last_edit"]) ) {
+            // abrechen im cms editor soll zur ursrungseite springen und nicht in den fileed
+            $_SESSION["page"] = $_SESSION["cms_last_referer"];
+            $ausgaben["cmslink"] = "<a href=\"".$_SESSION["cms_last_edit"]."?referer=".$_SESSION["cms_last_referer"]."\">#(cmslink)</a>";
+        } else {
+            $ausgaben["cmslink"] = "";
+        }
+
+        // bearbeiten- und loeschen link erstellen
+        if ( count($_SESSION["file_memo"]) >= 1 ) {
+            $ausgaben["fileedit"] = "<a href=\"".$cfg["basis"]."/edit.html\">#(fileedit)</a>";
+            $ausgaben["filedelete"] = "<a href=\"".$cfg["basis"]."/delete.html\">#(filedelete)</a>";
+        } else {
+            $ausgaben["fileedit"] = "";
+            $ausgaben["filedelete"] = "";
+        }
+
+        // +++
+        // funktions bereich ( aufbau )
+
+
+
+        // funktions bereich ( auswertung )
+        // ***
+
+        // where init
+        $part = array();
+
+        // suche verarbeiten
+        if ( isset($HTTP_GET_VARS["search"]) ) {
+            $_SESSION["fileed_position"] = 0;
+            $_SESSION["fileed_search"] = $HTTP_GET_VARS["search"];
+        } elseif ( isset($HTTP_GET_VARS["search"]) && $HTTP_GET_VARS["search"] == "" ) {
+            unset($_SESSION["fileed_search"]);
+        }
+        if ( $_SESSION["fileed_search"] ) {
+            $ausgaben["search"] = $_SESSION["fileed_search"];
+            $ausgaben["result"] = "#(answera) \"".$_SESSION["fileed_search"]."\" #(answerb) ";
+            $array1 = explode( " ", $_SESSION["fileed_search"] );
+            $array2 = array( "ffname", "fdesc", "fhit" );
+
+            foreach ( $array1 as $value1 ) {
+                if ( $value1 != "" ) {
+                    foreach ( $array2 as $value2 ) {
+                        if ( $part["search"] != "" ) $part["search"] .= " or ";
+                        $part["search"] .= $value2. " LIKE '%".$value1."%'";
+                    }
+                }
+            }
+            if ( $part["search"] != "" ) $part["search"] = "(".$part["search"].")";
+        } else {
+            $ausgaben["search"] = "";
+        }
+
+        // auswahlliste 1 verarbeiten
+        switch ( $_SESSION["fileed_filter0"] ) {
+            case 2:
+                #$part["auswahl1"] = "";
+                break;
+            case 1:
+                $part["auswahl1"] = " fdid = '".$_SESSION["custom"]."'";
+                break;
+            default:
+                $part["auswahl1"] = " fuid = '".$_SESSION["uid"]."'";
+        }
+
+        // auswahlliste 2 verarbeiten
+        switch ( $_SESSION["fileed_filter1"] ) {
+            case 2:
+                $part["auswahl2"] = " ffart in ('zip','bz2','gz')";
+                $hidedata["other"] = array();
+                break;
+            case 1:
+                $part["auswahl2"] = " ffart in ('pdf','odt','ods','odp')";
+                $hidedata["other"] = array();
+                break;
+            default:
+                $part["auswahl2"] = " ffart in ('gif','jpg','png')";
+                $hidedata["images"] = array();
+        }
+
+        // where build
+        if ( count($part) >= 2 ) $binder = " AND ";
+        foreach ( $part as $value ) {
+            if ( $where == "" ) {
+                $where = " WHERE ".$value;
+            } else {
+                $where .= $binder.$value;
+            }
+        }
+
+        // +++
+        // funktions bereich ( auswertung )
+
+
+
+        // funktions bereich
+        // ***
+
+        // db query
+        $sql = "SELECT *
+                  FROM ".$cfg["db"]["file"]["entries"]."
+                  ".$where."
+              ORDER BY ".$cfg["db"]["file"]["order"];
+        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+
+        // seiten umschalter
+        if ( $environment["parameter"][1] != "" ) {
+            $_SESSION["fileed_position"] = $environment["parameter"][1];
+        }
+        $inhalt_selector = inhalt_selector( $sql, $_SESSION["fileed_position"], $cfg["db"]["file"]["rows"], Null, 1, 3, Null );
+        $ausgaben["inhalt_selector"] = $inhalt_selector[0];
+        $sql = $inhalt_selector[1];
+        $ausgaben["anzahl"] = $inhalt_selector[2];
+
+        $result = $db -> query($sql); $i = 0;
+        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+
+        if ( $db->num_rows($result) == 0 ) {
+            #$ausgaben["result"] .= " keine Einträge gefunden.";
+            $ausgaben["result"] .= " #(answerc_no)";
+        } else {
+            // nur erweitern wenn bereits was drin steht
+            if ( $ausgaben["result"] ) {
+                #$ausgaben["result"] .= " folgende Einträge gefunden.";
+                $ausgaben["result"] .= " #(answerc_yes)";
+            } else {
+                $ausgaben["result"]  = "";
+            }
+        }
+
+        if ( $getvalues != "" ) $getvalues = "?".$getvalues;
+        while ( $data = $db -> fetch_array($result,1) ) {
+
+            if (is_array($_SESSION["file_memo"])) {
+                if (in_array($data["fid"],$_SESSION["file_memo"])) {
+                    $cb = "<a href=".$cfg["basis"]."/list,".$environment["parameter"][1].",".$data["fid"].".html".$getvalues."><img width=\"13\" height\"13\" border=\"0\" src=\"".$cfg["iconpath"]."cms-cb1.png\"></a>";
+                } else {
+                    $cb = "<a href=".$cfg["basis"]."/list,".$environment["parameter"][1].",".$data["fid"].".html".$getvalues."><img width=\"13\" height\"13\" border=\"0\" src=\"".$cfg["iconpath"]."cms-cb0.png\"></a>";
+                }
+            } else {
+                $cb = "<a href=".$cfg["basis"]."/list,".$environment["parameter"][1].",".$data["fid"].".html".$getvalues."><img width=\"13\" height\"13\" border=\"0\" src=".$cfg["iconpath"]."cms-cb0.png border=0></a>";
+            }
+
+            // tabellen farben wechseln
+            if ( $cfg["color"]["set"] == $cfg["color"]["a"]) {
+                $cfg["color"]["set"] = $cfg["color"]["b"];
+            } else {
+                $cfg["color"]["set"] = $cfg["color"]["a"];
+            }
+            $dataloop["list"][$data["fid"]]["color"] = $cfg["color"]["set"];
+
+            $dataloop["list"][$data["fid"]]["ehref"] = "edit,".$data["fid"].".html";
+
+            $type = $cfg["filetyp"][$data["ffart"]];
+            $dataloop["list"][$data["fid"]]["dhref"] = $pathvars["filebase"]["webdir"].
+                                                       $pathvars["filebase"][$cfg["fileopt"][$type]["name"]].
+                                                       $cfg["fileopt"][$type]["name"]."_".
+                                                       $data["fid"].".".$data["ffart"];
+            if ( $data["ffart"] == "pdf" ) {
+                $dataloop["list"][$data["fid"]]["dtarget"] = "_blank";
+            } else {
+                $dataloop["list"][$data["fid"]]["dtarget"] = "";
+            }
+
+
+            $dataloop["list"][$data["fid"]]["src"] = $pathvars["filebase"]["webdir"].
+                                                     $pathvars["filebase"]["pic"]["root"].
+                                                     $pathvars["filebase"]["pic"]["tn"]."tn_".
+                                                     $data["fid"].".".$data["ffart"];
+
+            $dataloop["list"][$data["fid"]]["alt"] = $data["ffname"];
+            $dataloop["list"][$data["fid"]]["title"] = $data["ffname"];
+
+            $dataloop["list"][$data["fid"]]["cb"] = $cb;
+
+            $dataloop["list"][$data["fid"]]["ohref"] = "list/view,o,".$data["fid"].".html";
+            $dataloop["list"][$data["fid"]]["bhref"] = "list/view,b,".$data["fid"].".html";
+            $dataloop["list"][$data["fid"]]["mhref"] = "list/view,m,".$data["fid"].".html";
+            $dataloop["list"][$data["fid"]]["shref"] = "list/view,s,".$data["fid"].".html";
+
+            $i++;
+            $even = $i / $cfg["db"]["file"]["line"];
+            if ( is_int($even) ) {
+                $dataloop["list"][$data["fid"]]["newline"] = $cfg["db"]["file"]["newline"];
+            } else {
+                $dataloop["list"][$data["fid"]]["newline"] = "";
+            }
+        }
+
+        // +++
+        // funktions bereich
+
+
+        // page basics
+        // ***
+
+        // fehlermeldungen
+        if ( $HTTP_GET_VARS["error"] != "" ) {
+            if ( $HTTP_GET_VARS["error"] == 1 ) {
+                $ausgaben["form_error"] = "#(error1)";
+            } else {
+                $ausgaben["form_error"] = "#(error2)";
+            }
+        } else {
+            $ausgaben["form_error"] = "";
+        }
+
+        // navigation erstellen
+        $ausgaben["link_new"] = $cfg["basis"]."/add.html";
+
+        // hidden values
+        #$ausgaben["form_hidden"] .= "";
+
+        // was anzeigen
+        $cfg["path"] = str_replace($pathvars["virtual"],"",$cfg["basis"]);
+        $mapping["main"] = crc32($cfg["path"]).".list";
+        #$mapping["navi"] = "leer";
+
+        // unzugaengliche #(marken) sichtbar machen
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $ausgaben["inaccessible"] = "inaccessible values:<br />";
+            $ausgaben["inaccessible"] .= "# (error1) #(error1)<br />";
+            $ausgaben["inaccessible"] .= "# (error2) #(error2)<br />";
+
+            $ausgaben["inaccessible"] .= "# (cmslink) #(cmslink)<br />";
+            $ausgaben["inaccessible"] .= "# (fileedit) #(fileedit)<br />";
+            $ausgaben["inaccessible"] .= "# (filedelete) #(filedelete)<br />";
+
+            $ausgaben["inaccessible"] .= "# (answera) #(answera)<br />";
+            $ausgaben["inaccessible"] .= "# (answerb) #(answerb)<br />";
+            $ausgaben["inaccessible"] .= "# (answerc_no) #(answerc_no)<br />";
+            $ausgaben["inaccessible"] .= "# (answerc_yes) #(answerc_yes)<br />";
+
+            $ausgaben["inaccessible"] .= "# (prev) #(next)<br />";
+            $ausgaben["inaccessible"] .= "# (next) #(prev)<br />";
+        } else {
+            $ausgaben["inaccessible"] = "";
+        }
+
+        // wohin schicken
+        $ausgaben["form1_aktion"] = $cfg["basis"]."/list.html";
+        $ausgaben["form2_aktion"] = $cfg["basis"]."/upload.html";
+
+        // +++
+        // page basics
+
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
