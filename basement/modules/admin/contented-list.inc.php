d7b71d5c54806e0a6a9e565ab280e23903142579
werner-a <chaot@chaos.de>
2007-11-15 08:00:57 +0000

    versionierung preview

---
+++ basement/modules/admin/contented-list.inc.php
@@ -0,0 +1,137 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: leer-list.inc.php 738 2007-09-13 11:28:23Z chaot $";
+// "leer - list funktion";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001-2007 Werner Ammon ( wa<at>chaos.de )
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    if ( $cfg["right"] == "" || $rechte[$cfg["right"]] == -1 ) {
+
+        // funktions bereich
+        // ***
+
+        ### put your code here ###
+
+        /* z.B. db query */
+        $sql = "SELECT version, html, content, changed, byalias
+                    FROM ". SITETEXT ."
+                    WHERE lang = '".$environment["language"]."'
+                    AND label ='".$environment["parameter"][3]."'
+                    AND tname ='".$environment["parameter"][2]."'
+                    ORDER BY version DESC";
+        if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
+
+        // seiten umschalter
+        $inhalt_selector = inhalt_selector( $sql, $environment["parameter"][1], $cfg["db"]["leer"]["rows"], $parameter, 1, 3, $getvalues );
+        $ausgaben["inhalt_selector"] = $inhalt_selector[0]."<br />";
+        $sql = $inhalt_selector[1];
+        $ausgaben["anzahl"] = $inhalt_selector[2];
+
+        $result = $db -> query($sql);
+        while ( $data = $db -> fetch_array($result,1) ) {
+            $dataloop["list"][$data["version"]][0] = $data["changed"];
+            $dataloop["list"][$data["version"]][1] = $data["version"];
+
+            // tabellen farben wechseln
+            if ( $cfg["color"]["set"] == $cfg["color"]["a"]) {
+                $cfg["color"]["set"] = $cfg["color"]["b"];
+            } else {
+                $cfg["color"]["set"] = $cfg["color"]["a"];
+            }
+
+/*            $dataloop["list"][$data["id"]] = array(
+                                   "color" => $cfg["color"]["set"],
+                                  "field1" => $data["field1"],
+                                    "edit" => $cfg["basis"]."/edit,".$data["id"].".html",
+                                  "delete" => $cfg["basis"]."/delete,".$data["id"].".html",
+                                 "details" => $cfg["basis"]."/details,".$data["id"].".html",
+            );*/
+        }
+        #echo sprintf("<pre>%s</pre>",print_r($dataloop,True));
+        // +++
+        // funktions bereich
+
+
+        // page basics
+        // ***
+
+        // fehlermeldungen
+        if ( $HTTP_GET_VARS["error"] != "" ) {
+            if ( $HTTP_GET_VARS["error"] == 1 ) {
+                $ausgaben["form_error"] = "#(error1)";
+            }
+        } else {
+            $ausgaben["form_error"] = "";
+        }
+
+        // navigation erstellen
+        $ausgaben["link_new"] = $cfg["basis"]."/add.html";
+
+        // hidden values
+        #$ausgaben["form_hidden"] .= "";
+
+        // was anzeigen
+        $cfg["path"] = str_replace($pathvars["virtual"],"",$cfg["basis"]);
+        $mapping["main"] = crc32($cfg["path"]).".list";
+        #$mapping["navi"] = "leer";
+
+        // unzugaengliche #(marken) sichtbar machen
+        if ( isset($HTTP_GET_VARS["edit"]) ) {
+            $ausgaben["inaccessible"] = "inaccessible values:<br />";
+            $ausgaben["inaccessible"] .= "# (error1) #(error1)<br />";
+            $ausgaben["inaccessible"] .= "# (edittitel) #(edittitel)<br />";
+            $ausgaben["inaccessible"] .= "# (deletetitel) #(deletetitel)<br />";
+        } else {
+            $ausgaben["inaccessible"] = "";
+        }
+
+        // wohin schicken
+        #n/a
+
+        // +++
+        // page basics
+
+    } else {
+        header("Location: ".$pathvars["virtual"]."/");
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+?>
