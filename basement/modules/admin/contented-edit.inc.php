65b0642e3c926f9f775b1b80687aae5775806cbf
werner-a <chaot@chaos.de>
2007-11-20 11:34:32 +0000

    tag schutz fix aus -1- wird ::1:: u.s.w.

--- basement/modules/admin/contented-edit.inc.php
+++ basement/modules/admin/contented-edit.inc.php
@@ -242,7 +242,7 @@
 
         // eWeBuKi tag schutz part 3
         $mark_o = array( "#(", "g(", "#{", "!#" );
-        $hide_o = array( "-1-", "-2-", "-3-", "-4-" );
+        $hide_o = array( "::1::", "::2::", "::3::", "::4::" );
         $form_values["content"] = str_replace( $mark_o, $hide_o, $form_values["content"]);
 
 
