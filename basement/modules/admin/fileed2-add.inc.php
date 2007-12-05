1930d8b1347c301479ad2157da1828219dbac7ff
EmNiLi <buffy@level14.de>
2007-12-05 11:49:41 +0000

    nun funktioniert der filled auch mit den neuen rechten

--- basement/modules/admin/fileed2-add.inc.php
+++ basement/modules/admin/fileed2-add.inc.php
@@ -43,7 +43,9 @@
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    if ( $cfg["right"] == "" || $rechte[$cfg["right"]] == -1 ) {
+    if ( $cfg["right"] == "" ||
+        priv_check("/".$cfg["subdir"]."/".$cfg["name"],$cfg["right"]) ||
+        priv_check_old("",$cfg["right"]) ) {
 
         // auf session losgehen, falls zip bearbeitet wurde
         if ( count($_SESSION["zip_extracted"]) == 0 ) unset($_SESSION["zip_extracted"]);
