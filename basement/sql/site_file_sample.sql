39a295bb70089e47e876dcc05096181c361dcecc
werner-a <chaot@chaos.de>
2008-01-17 18:10:39 +0000

    beispiel dateien ausgelagert nach trunk/sample/site_file_sample.tar.bz2 und sql/site_file_sample.sql. fuer die beispiel dateien einfach archiv im ewebuki dir entpacken und sql einspielen.

---
+++ basement/sql/site_file_sample.sql
@@ -0,0 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 17, 2008 at 07:04 PM
+-- Server version: 4.0.24
+-- PHP Version: 5.2.0-8+etch7~bpo.1
+
+
+--
+-- Database: `ewebuki_mdebase`
+--
+
+--
+-- Dumping data for table `site_file`
+--
+
+INSERT INTO `site_file` (`fid`, `frefid`, `fuid`, `fdid`, `ftname`, `ffname`, `ffart`, `fdesc`, `funder`, `fhit`, `fdel`) VALUES
+(1, 0, 1, 0, '', 'ewebuki_160x67.png', 'png', 'eWeBuKi Logo Beschreibung', 'eWeBuKi Logo Unterschift', '', NULL),
+(2, 0, 1, 0, '', 'alex.jpg', 'jpg', 'Der Alex in Berlin', 'Der Alex in Berlin', '#p2,10# #p1,10#', NULL),
+(3, 0, 1, 0, '', 'dark.jpg', 'jpg', 'Unwetter naht', ' Unwetter naht', '#p1,20#', NULL),
+(4, 0, 1, 0, '', 'dust.jpg', 'jpg', 'Staub im PC', 'Staub im PC', '#p2,20#', NULL),
+(5, 0, 1, 0, '', 'hummel.jpg', 'jpg', 'Hummelflug', 'Hummelflug', '#p2,30#', NULL),
+(6, 0, 1, 0, '', 'italy.jpg', 'jpg', 'Süditalien', 'Süditalien', '#p1,30#', NULL),
+(7, 0, 1, 0, '', 'karibik.jpg', 'jpg', 'Karibik Blick', 'Karibik Blick', '#p1,40#', NULL),
+(8, 0, 1, 0, '', 'palenque.jpg', 'jpg', 'Maya Ruine', 'Maya Ruine', '#p1,50#', NULL),
+(9, 0, 1, 0, '', 'rose.jpg', 'jpg', 'Rose blüht', 'Rose blüht', '#p2,40#', NULL),
+(10, 0, 1, 0, '', 'wolken.jpg', 'jpg', 'Wolkenblick', 'Wolkenblick', '#p1,60#', NULL);
