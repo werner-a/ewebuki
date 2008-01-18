91285dfa3cd9bed74e025f47e58fb3acfd0c74cc
EmNiLi <buffy@level14.de>
2008-01-18 15:19:26 +0000

    beispiel dateien ausgelagert nach trunk/sample/site_file_sample.tar.bz2 und sql/site_file_sample.psql.sql. fuer die beispiel dateien einfach archiv im ewebuki dir entpacken und sql einspielen.

---
+++ basement/sql/site_file_sample.psql.sql
@@ -0,0 +1,15 @@
+
+--
+-- Dumping data for table `site_file`
+--
+
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(1, 0, 1, 0, '', 'ewebuki_160x67.png', 'png', 'eWeBuKi Logo Beschreibung', 'eWeBuKi Logo Unterschift', '', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(2, 0, 1, 0, '', 'alex.jpg', 'jpg', 'Der Alex in Berlin', 'Der Alex in Berlin', '#p2,10# #p1,10#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(3, 0, 1, 0, '', 'dark.jpg', 'jpg', 'Unwetter naht', ' Unwetter naht', '#p1,20#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(4, 0, 1, 0, '', 'dust.jpg', 'jpg', 'Staub im PC', 'Staub im PC', '#p2,20#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(5, 0, 1, 0, '', 'hummel.jpg', 'jpg', 'Hummelflug', 'Hummelflug', '#p2,30#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(6, 0, 1, 0, '', 'italy.jpg', 'jpg', 'Süditalien', 'Süditalien', '#p1,30#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(7, 0, 1, 0, '', 'karibik.jpg', 'jpg', 'Karibik Blick', 'Karibik Blick', '#p1,40#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(8, 0, 1, 0, '', 'palenque.jpg', 'jpg', 'Maya Ruine', 'Maya Ruine', '#p1,50#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(9, 0, 1, 0, '', 'rose.jpg', 'jpg', 'Rose blüht', 'Rose blüht', '#p2,40#', NULL);
+INSERT INTO site_file (fid, frefid, fuid, fdid, ftname, ffname, ffart, fdesc, funder, fhit, fdel) VALUES(10, 0, 1, 0, '', 'wolken.jpg', 'jpg', 'Wolkenblick', 'Wolkenblick', '#p1,60#', NULL);
