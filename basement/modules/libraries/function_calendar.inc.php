bb245d42844a0e55e4e582641845dbd1dcac4aaf
EmNiLi <buffy@level14.de>
2008-02-29 11:58:24 +0000

    nun mit kopf

--- basement/modules/libraries/function_calendar.inc.php
+++ basement/modules/libraries/function_calendar.inc.php
@@ -1,4 +1,48 @@
 <?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// "$Id: function_calendar.inc.php 1131 2007-12-12 08:45:50Z chaot $";
+// "funktion loader";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    eWeBuKi - a easy website building kit
+    Copyright (C)2001-2007 Werner Ammon ( wa<at>chaos.de )
+
+    This script is a part of eWeBuKi
+
+    eWeBuKi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    eWeBuKi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eWeBuKi; If you did not, you may download a copy at:
+
+    URL:  http://www.gnu.org/licenses/gpl.txt
+
+    You may also request a copy from:
+
+    Free Software Foundation, Inc.
+    59 Temple Place, Suite 330
+    Boston, MA 02111-1307
+    USA
+
+    You may contact the author/development team at:
+
+    Chaos Networks
+    c/o Werner Ammon
+    Lerchenstr. 11c
+
+    86343 Königsbrunn
+
+    URL: http://www.chaos.de
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function calendar($tag="") {
 
     $tage = array("So", "Mo", "Di", "Mi","Do", "Fr", "Sa");
@@ -51,5 +95,8 @@ function calendar($tag="") {
     $ausgabe .= "</table>";
 
     return $ausgabe;
+
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
