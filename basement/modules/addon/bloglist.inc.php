2145398cc4f755a352daa3f5454cc85ea26a035d
EmNiLi <buffy@level14.de>
2008-06-24 08:08:10 +0000

    auf $_GET umgestellt

--- basement/modules/addon/bloglist.inc.php
+++ basement/modules/addon/bloglist.inc.php
@@ -52,7 +52,7 @@
     if ( $cfg["bloglist"]["iconpath"] == "" ) $cfg["bloglist"]["iconpath"] = "/images/default/";
 
     // label bearbeitung aktivieren
-    if ( isset($HTTP_GET_VARS["edit"]) ) {
+    if ( isset($_GET["edit"]) ) {
         $specialvars["editlock"] = 0;
     } else {
         $specialvars["editlock"] = -1;
