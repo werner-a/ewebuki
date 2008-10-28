a3723b0bd87bb28e1f3f40a34c71222f72e24c5c
EmNiLi <buffy@level14.de>
2008-10-28 13:52:18 +0000

    ansicht der versionen an view-recht gekoppelt

--- basement/basic/libraries/function_content.inc.php
+++ basement/basic/libraries/function_content.inc.php
@@ -85,7 +85,7 @@
 
             if ( strpos($label,",") !== false ) break; // javascript fix
 
-            if ( preg_match("/^v[0-9]*$/",$environment["parameter"][1],$regs) ) {
+            if ( preg_match("/^v[0-9]*$/",$environment["parameter"][1],$regs) && priv_check($environment["ebene"]."/".$environment["kategorie"],"view")  ) {
                 $version_sql = "AND version=".substr($environment["parameter"][1],1);
                 $version = substr($environment["parameter"][1],1);
             } else {
