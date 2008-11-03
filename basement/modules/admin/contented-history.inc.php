8e2ca581be284e432354b3b636faff45e693d7f5
Krompi <krompi@gmail.com>
2008-11-03 12:27:38 +0000

    auch die diff-methode kann ueber parameter gesteuert werden

--- basement/modules/admin/contented-history.inc.php
+++ basement/modules/admin/contented-history.inc.php
@@ -65,7 +65,11 @@
         }
 
         // label steuerung wenn kein para dann wird default-label aus cfg hergenommen
-        if ( $environment["parameter"][3] == "" )  $environment["parameter"][3] = $cfg["contented"]["default_label"];
+        if ( $environment["parameter"][3] == "" ) {
+            $label = $cfg["contented"]["default_label"];
+        } else {
+            $label = $environment["parameter"][3];
+        }
 
         $old = $environment["parameter"][4];
         $new = $environment["parameter"][5];
@@ -77,7 +81,7 @@
             if ( $_POST["new"] || $_GET["new"] ) {
                 $_POST["new"] != "" ? $new = $_POST["new"] : $new = $_GET["new"];
             }
-            header("Location: ".$cfg["contented"]["basis"]."/history,".$environment["parameter"][1].",".$environment["parameter"][2].",".$environment["parameter"][3].",".$old.",".$new.",".$environment["parameter"][6].",verify.html");
+            header("Location: ".$cfg["contented"]["basis"]."/history,".$environment["parameter"][1].",".$tname.",".$label.",".$old.",".$new.",".$environment["parameter"][6].",".$environment["parameter"][7].",verify.html");
         }
 
         // page basics
@@ -89,7 +93,7 @@
         $sql = "SELECT max(version), min(version)
                   FROM site_text
                  WHERE tname='".$tname."'
-                   AND label='".$environment["parameter"][3]."'";
+                   AND label='".$label."'";
         $result = $db -> query($sql);
         $data = $db -> fetch_array($result,1);
         $last_version = $data["max"];
@@ -98,13 +102,13 @@
         $sql = "SELECT *
                   FROM site_text
                  WHERE tname='".$tname."'
-                   AND label='".$environment["parameter"][3]."'
+                   AND label='".$label."'
               ORDER BY version DESC";
         if ( $debugging["sql_enable"] ) $debugging["ausgabe"] .= "sql: ".$sql.$debugging["char"];
         $result = $db -> query($sql);
 
         // Inhalt Selector erstellen und SQL modifizieren
-        $parameter = ",".$environment["parameter"][2].",".$environment["parameter"][3].",".$environment["parameter"][4].",".$environment["parameter"][5].",".$environment["parameter"][6];
+        $parameter = ",".$tname.",".$label.",".$old.",".$new.",".$environment["parameter"][6].",".$environment["parameter"][7];
         $inhalt_selector = inhalt_selector( $sql, $position, $cfg["contented"]["history_rows"], $parameter );
         $ausgaben["inhalt_selector"] .= $inhalt_selector[0];
         $sql = $inhalt_selector[1];
@@ -131,7 +135,7 @@
             $dataloop["list"][$form_values["version"]] = array(
                              "url" => $pathvars["virtual"].$pfad.",v".$form_values["version"].".html",
                             "date" => substr($form_values["changed"],8,2).". ".gerdate("gml",substr($form_values["changed"],5,2))." ".substr($form_values["changed"],0,4)." ".substr($form_values["changed"],11,5),
-                            "name" => $form_values["bysurname"]." ".$form_values["byforename"]." (".$form_values["version"].")",
+                            "name" => $form_values["bysurname"]." ".$form_values["byforename"],
                              "cb1" => $form_values["version"],
                              "cb2" => $form_values["version"],
                      "visible_old" => "visible",
@@ -158,23 +162,24 @@
         }
 
         // hier erfolgt der diff
-        if ( $environment["parameter"][4] && $environment["parameter"][5] ) {
+        if ( $old != "" && $new != "" ) {
 
             $sql = "SELECT content,version
                       FROM site_text
                      WHERE tname='".$tname."'
-                       AND label='".$environment["parameter"][3]."'
+                       AND label='".$label."'
                        AND version=".$old;
             $result = $db -> query($sql);
             $data_old = $db -> fetch_array($result,1);
             $sql = "SELECT content,version
                       FROM site_text
                      WHERE tname='".$tname."'
-                       AND label='".$environment["parameter"][3]."'
+                       AND label='".$label."'
                        AND version=".$new;
             $result = $db -> query($sql);
             $data_new = $db -> fetch_array($result,1);
 
+            // tagreplace?
             if ( $environment["parameter"][6] == "html" ) {
                 if ( $data_new["version"] > $data_old["version"] ) {
                     $first = tagreplace($data_new["content"]);
@@ -197,18 +202,23 @@
             $old_array = explode("\n", $second);
             $new_array = explode("\n", $first);
 
-            if ( strstr($cfg["contented"]["diff_engine"],"phpdiff") ) {
-                if ( $cfg["contented"]["diff_engine"] == "phpdiff3" ) {
-                    $diff = phpdiff3($second,$first);
-                } else {
-                    $diff = arr_diff($old_array,$new_array);
-                }
-                $ausgaben["diff"] = $diff;
+            // diff-methode festlegen
+            if ( $environment["parameter"][7] != "" ) {
+                $diff_type = $environment["parameter"][7];
+            } else {
+                $diff_type = $cfg["contented"]["diff_engine"];
+            }
+            if ( $diff_type == "phpdiff3" ) {
+                $diff = phpdiff3($second,$first);
+            } elseif ( strstr($diff_type,"phpdiff") ) {
+                $diff = arr_diff($old_array,$new_array);
             } else {
                 $diff = new Text_Diff('auto', array($old_array, $new_array));
                 $renderer = new Text_Diff_Renderer_inline();
-                $ausgaben["diff"] = str_replace("\n","<br>",$renderer->render($diff));
+                $diff = str_replace("\n","<br>",$renderer->render($diff));
             }
+            $ausgaben["diff"] = $diff;
+
         }
 
         // form options holen
@@ -241,7 +251,7 @@
         $ausgaben["form_error"] = "";
 
         // navigation erstellen
-        $ausgaben["form_aktion"] = $cfg["contented"]["basis"]."/history,".$environment["parameter"][1].",".$tname.",".$environment["parameter"][3].",".$environment["parameter"][4].",".$environment["parameter"][5].",".$environment["parameter"][6].",verify.html";
+        $ausgaben["form_aktion"] = $cfg["contented"]["basis"]."/history,".$environment["parameter"][1].",".$tname.",".$label.",".$old.",".$new.",".$environment["parameter"][6].",".$environment["parameter"][7].",verify.html";
         $ausgaben["form_break"] = $cfg["contented"]["basis"]."/list.html";
 
         // hidden values
