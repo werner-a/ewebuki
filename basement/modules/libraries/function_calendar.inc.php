56def3a215d58add3a071eb60ac5565da5c975e3
EmNiLi <buffy@level14.de>
2008-11-26 14:21:29 +0000

    bugfix

--- basement/modules/libraries/function_calendar.inc.php
+++ basement/modules/libraries/function_calendar.inc.php
@@ -91,7 +91,7 @@ function calendar($monat="",$jahr="",$class="",$extendend="",$linked="",$no_secu
         $jump_forward = "<a href=\"".$environment["parameter"][0].$protect_parameter.",".$forward.",01.html\">";
         if ( $no_secure == "" ) {
             if ( $environment["parameter"][$start_parameter+1] && abs($aktuell["year"] - $environment["parameter"][$start_parameter+1]) > 1) {
-                header("Location: ".str_replace(strstr($pathvars["uri"],"?"),"",$pathvars["uri"]));
+                header("Location: ".$pathvars["virtual"]."/index.html");
             } elseif ( $jahr-1-$aktuell["year"] < -1) {
                     $jump_back = "";
             } elseif ( $jahr+1-$aktuell["year"] > 1) {
