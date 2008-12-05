3444df0097aa166a48857b1856b6fb583e6f9a71
EmNiLi <buffy@level14.de>
2008-12-05 08:06:20 +0000

    header jetzt richtig bei kategorie

--- basement/modules/admin/bloged-add.inc.php
+++ basement/modules/admin/bloged-add.inc.php
@@ -108,8 +108,7 @@
             $content  = "[!][".$cfg["bloged"]["blogs"][$ebene]["sort"][0]."]".$sort."[/".$cfg["bloged"]["blogs"][$ebene]["sort"][0]."]".$kategorie;
 
             if ( is_array($cfg["bloged"]["blogs"][$ebene]["addons"]) ) {
-                if ( $_POST["kategorie"] != "" ) $ebene = $_POST["kategorie"];
-                $header = $pathvars["virtual"].$ebene.".html";
+                $header = $pathvars["virtual"].$_POST["kategorie"].".html";
                 foreach ( $cfg["bloged"]["blogs"][$ebene]["addons"] as $key => $value ) {
                     if ( $value == "SORT" ) continue;
                     if ( !is_array($value) ) {
