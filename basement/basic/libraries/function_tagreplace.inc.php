1a61c5474d3b6fda0d3d5b22ef02ca4a7eba788c
EmNiLi <buffy@level14.de>
2009-06-03 14:26:22 +0000

    bugfix: nlreplace nur auf tagwert anwenden, nicht auf gesamten content

--- basement/basic/libraries/function_tagreplace.inc.php
+++ basement/basic/libraries/function_tagreplace.inc.php
@@ -253,7 +253,7 @@
                         $replace = str_replace($opentag.$tagoriginal.$closetag,$defaults["tag"]["hr"].$tagwert.$defaults["tag"]["/hr"],$replace);
                         break;
                     case "[/TAB]":
-                        if ( $specialvars["newbrmode"] == True ) $replace = str_replace("\r\n","",$replace);
+                        if ( $specialvars["newbrmode"] == True ) $tagwert = str_replace("\r\n","",$tagwert);
                         if ( $sign == "]" ) {
                             $replace = str_replace($opentag.$tagoriginal.$closetag,"<table cellspacing=\"0\" cellpadding=\"1\">".$tagwert."</table>",$replace);
                         } else {
