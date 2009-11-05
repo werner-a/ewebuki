f916db2a5ef2f5f8c46792caad1101d55c354acc
EmNiLi <buffy@level14.de>
2009-11-05 13:11:07 +0000

    radiobuttons beim moven auch bei alten rechten aktiv

--- basement/modules/libraries/function_menutree.inc.php
+++ basement/modules/libraries/function_menutree.inc.php
@@ -257,7 +257,7 @@
 
             // refid radio button
             if ( $radiorefid != "" ) {
-                if ( ($array["mid"] == $environment["parameter"][2]) || !priv_check(make_ebene($array["mid"]),$cfg["menued"]["modify"]["move"][2])  ) {
+                if ( $array["mid"] == $environment["parameter"][2] || ( $specialvars["security"]["new"] == -1 && !priv_check(make_ebene($array["mid"]),$cfg["menued"]["modify"]["move"][2]) ) ) {
                     $radio_disabled = " disabled";
                 } else {
                     $radio_disabled = "";
