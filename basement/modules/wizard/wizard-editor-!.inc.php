aa111d44ab17bce6472b4f5b169c6842493c9fab
EmNiLi <buffy@level14.de>
2010-02-04 08:11:10 +0000

    anzeigedatum unbegrenzt

--- basement/modules/wizard/wizard-editor-!.inc.php
+++ basement/modules/wizard/wizard-editor-!.inc.php
@@ -118,16 +118,12 @@
         // bei globalen artikeln nichts moeglich
         if ( $regs[2][1] == "/aktuell/archiv" ) {
             $hidedata["artikel"]["button1_display"] = "display:none";
-            $hidedata["artikel"]["button2_display"] = "display:none";
         }
 
         foreach ( $regs[1] as $key => $value ) {
             if ( $value == "KATEGORIE" ) continue;
-            if  ( $_GET[$value] ) {
-                $regs[2][$key] = $_GET[$value];
-            }
 
-            // variablen erzeugen
+            // tag_meat fuellen
             $$value = $regs[2][$key];
             if ( $_POST["send"] ) {
                 if ( $value == "ENDE" || $value == "SORT" ) {
@@ -141,24 +137,14 @@
         $SORT = substr($SORT,8,2).".".substr($SORT,5,2).".".substr($SORT,0,4);
         $ENDE = substr($ENDE,0,10);
         $ENDE = substr($ENDE,8,2).".".substr($ENDE,5,2).".".substr($ENDE,0,4);
-        if ( $ENDE != "01.01.1970" ) {
-            $display = "";
-        } else {
-            $display = "none";
-        }
-        if ( $ENDE == ".." ) {
-            $hidedata["artikel"]["button2_display"] = "display:none";
-            $display = "none";
-        }
 
-        $hidedata["artikel"]["display"] = $display;
         $hidedata["artikel"]["sort"] = $SORT;
         $hidedata["artikel"]["ende"] = $ENDE;
         $sort_timestamp = mktime(0,0,0,substr($SORT,3,2),substr($SORT,0,2),substr($SORT,6,4));
         $now_timestamp = mktime(0,0,0,date('m'),date('d'),date('Y'));
         $postsort_timestamp = mktime(0,0,0,substr($_POST["SORT"],5,2),substr($_POST["SORT"],8,2),substr($_POST["SORT"],0,4));
 
-        $sql = "Select Cast(SUBSTR(content,POSITION('[SORT]' IN content)+6,POSITION('[/SORT]' IN content)-POSITION('[SORT]' IN content)-6) AS DATETIME ) AS date from site_text where status =1 AND tname ='".$environment["parameter"][2]."'";
+        $sql = "Select Cast(SUBSTR(content,POSITION('[SORT]' IN content)+6,POSITION('[/SORT]' IN content)-POSITION('[SORT]' IN content)-6) AS DATETIME ) AS date from site_text where tname ='".$environment["parameter"][2]."' ORDER BY version DESC";
         $result = $db -> query($sql);
         $data = $db -> fetch_array($result,1);
         if ( $db -> num_rows($result)  > 0 ) {
@@ -166,21 +152,18 @@
         }
 
         if ( $_POST["send"] ) {
-            if ( $regs[2][1] != "/aktuell/archiv" ) {
-                if ( $postsort_timestamp < $now_timestamp && ( $check_date == "" || $postsort_timestamp < $check_date )) {
-                    if ( $_POST["send"][0] == "Abschicken" ) {
-                        header("Location: ".$_SESSION["page"]."?error=1");
-                        exit;
-                    }
+            if ( $postsort_timestamp < $now_timestamp && ( $check_date == "" || $postsort_timestamp < $check_date )) {
+                if ( $_POST["send"][0] == "Abschicken" ) {
+                    header("Location: ".$_SESSION["page"]."?error=1");
+                    exit;
                 }
-                if ( $_POST["ENDE"] != "..") {
-                    $postend_timestamp = mktime(0,0,0,substr($_POST["ENDE"],5,2),substr($_POST["ENDE"],8,2),substr($_POST["ENDE"],0,4));
-                    $periode = $postend_timestamp-$postsort_timestamp;
-                    if ( ( $_POST["ENDE"] && $_POST["ENDE"] != "1970-01-01") && ($postend_timestamp <= $postsort_timestamp ) ) {
-                        if ( $_POST["send"][0] == "Abschicken" ) {
-                            header("Location: ".$_SESSION["page"]."?error=2");
-                            exit;
-                        }
+            }
+            if ( $_POST["ENDE"] != "1970-01-01") {
+                $postend_timestamp = mktime(0,0,0,substr($_POST["ENDE"],5,2),substr($_POST["ENDE"],8,2),substr($_POST["ENDE"],0,4));
+                if ( $postend_timestamp < $postsort_timestamp  ) {
+                    if ( $_POST["send"][0] == "Abschicken" ) {
+                        header("Location: ".$_SESSION["page"]."?error=2");
+                        exit;
                     }
                 }
             }
