e6bf99575f7cdde091b4c6729691af140bb4d22e
werner-a <chaot@chaos.de>
2015-03-10 16:35:07 +0000

    header and id fixes

--- basement/modules/admin/prived-edit.inc.php
+++ basement/modules/admin/prived-edit.inc.php
@@ -1,11 +1,11 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// "$Id: leer-edit.inc.php 1355 2008-05-29 12:38:53Z buffy1860 $";
+// "$Id$";
 // "leer - edit funktion";
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
     eWeBuKi - a easy website building kit
-    Copyright (C)2001-2007 Werner Ammon ( wa<at>chaos.de )
+    Copyright (C)2001-2015 Werner Ammon ( wa<at>chaos.de )
 
     This script is a part of eWeBuKi
 
@@ -37,7 +37,7 @@
     c/o Werner Ammon
     Lerchenstr. 11c
 
-    86343 K�nigsbrunn
+    86343 Koenigsbrunn
 
     URL: http://www.chaos.de
 */
