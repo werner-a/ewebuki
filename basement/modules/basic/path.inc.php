51ba654b6353243f27075b3781ee62e3321af9be
werner-a <chaot@chaos.de>
2015-06-16 15:46:12 +0000

    empty oder isset wirkt nicht bei null in mysql

--- basement/modules/basic/path.inc.php
+++ basement/modules/basic/path.inc.php
@@ -206,7 +206,7 @@
             }
 
             // pdf button disabled?               
-            if ( isset($data["disablepdf"]) && ( $data["disablepdf"] != -1 && $path == $environment["ebene"]."/".$environment["kategorie"] )) {
+            if ( array_key_exists("disablepdf", $data) && ( $data["disablepdf"] != -1 && $path == $environment["ebene"]."/".$environment["kategorie"] )) {
                 //<a href="/index.html?pdf=2" target="_blank" title="Link in neuem Fenster: /index.html?pdf=2" class="link_intern">PDF: pdf</a>
                 $ausgaben["pdfbutton0"] = $cfg["pdfc"]["buttons"]["b0"].$ausgaben["auth_url"].$cfg["pdfc"]["buttons"]["e0"];
                 $ausgaben["pdfbutton1"] = $cfg["pdfc"]["buttons"]["b1"].$ausgaben["auth_url"].$cfg["pdfc"]["buttons"]["e1"];
